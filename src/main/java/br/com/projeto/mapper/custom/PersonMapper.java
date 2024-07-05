f6e648ad71072158887273b19587d59f9ad2330b
henrique-amorim-one <78829197+henrique1023@users.noreply.github.com>
2024-07-05 14:40:51 -0300

    feat - adicionando projeto base

---
+++ src/main/java/br/com/projeto/mapper/custom/PersonMapper.java
@@ -0,0 +1,29 @@
+package br.com.projeto.mapper.custom;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonMapper {
+//
+//    public PersonVO convertEntityToVo(Person person){
+//        PersonVO vo = new PersonVO();
+//        vo.setKey(person.getId());
+//        vo.setAddress(person.getAddress());
+//        vo.setFirstName(person.getFirstName());
+//        vo.setLastName(person.getLastName());
+//        vo.setGender(person.getGender());
+//        return vo;
+//    }
+//
+//    public Person convertVoToEntity(PersonVO person){
+//        Person entity = new Person();
+//        entity.setId(person.getKey());
+//        entity.setAddress(person.getAddress());
+//        entity.setFirstName(person.getFirstName());
+//        entity.setLastName(person.getLastName());
+//        entity.setGender(person.getGender());
+////        entity.setBirthDay(new Date());
+//
+//        return entity;
+//    }
+}
