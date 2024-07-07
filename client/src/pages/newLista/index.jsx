56825ba0efd074864edd528f7030f8cf8ec55f9d
henrique-amorim-one <78829197+henrique1023@users.noreply.github.com>
2024-07-07 11:51:22 -0300

    feat - adiciona campo para musicas

--- client/src/pages/newLista/index.jsx
+++ client/src/pages/newLista/index.jsx
@@ -11,6 +11,12 @@ export default function NewLista(){
     const [id, setId] = useState(null)
     const [nome, setNome] = useState('')
     const [descricao, setDescricao] = useState('')
+    const [titulo, setTitulo] = useState('')
+    const [artista, setArtista] = useState('')
+    const [album, setAlbum] = useState('')
+    const [ano, setAno] = useState('')
+    const [genero, setGenero] = useState('')
+    const [musicas, setMusicas] = useState([])
 
     const navigate = useNavigate();
 
@@ -28,7 +34,6 @@ export default function NewLista(){
 
     async function loadBook(){
         try {
-            console.log(listName)
             const resp = await api.get(`api/lists/v1/${listName}`, { 
                 headers: {
                     Authorization: `Bearer ${accessToken}`
@@ -37,6 +42,7 @@ export default function NewLista(){
                 setId(resp.data.id)
                 setNome(resp.data.nome)
                 setDescricao(resp.data.descricao)
+                setMusicas(resp.data.musicas)
         } catch (error) {
             alert('Error load book' + error)
         }
@@ -48,6 +54,7 @@ export default function NewLista(){
         const data = {
             nome,
             descricao,
+            musicas: musicas
         }
 
         try {
@@ -72,6 +79,22 @@ export default function NewLista(){
         }
     }
 
+    const addMusicAndList = () => {
+        return async (e) => {
+            e.preventDefault();
+            const data = {
+                titulo,
+                artista,
+                album,
+                ano,
+                genero
+            }
+            var _musicas = [...musicas]
+            _musicas.push(data)
+            setMusicas(_musicas)
+        }
+    }
+
     return (
         <div className="new-book-container">
             <div className="content">
@@ -92,6 +115,44 @@ export default function NewLista(){
 
                     <button className="button" type="submit">{listName === '0' ? 'Add' : 'Edit'}</button>
                 </form>
+            </div>
+            <div className="content">
+                <form onSubmit={addMusicAndList()}>
+                    <input type="text" placeholder="Titulo" 
+                    value={titulo} onChange={e => setTitulo(e.target.value)}/>
+                    <input type="text" placeholder="Artista" 
+                    value={artista} onChange={e => setArtista(e.target.value)}/>
+                    <input type="text" placeholder="Album" 
+                    value={album} onChange={e => setAlbum(e.target.value)}/>
+                    <input type="text" placeholder="Ano" 
+                    value={ano} onChange={e => setAno(e.target.value)}/>
+                    <input type="text" placeholder="Genero" 
+                    value={genero} onChange={e => setGenero(e.target.value)}/>
+                    <button className="button" type="submit">Add music</button>
+                </form>
+                <section className="form">
+                    {musicas !== undefined ? (
+                    <div>
+                        <strong>Músicas:</strong>
+                        <ul>
+                            {musicas.map(musica => (
+                                <li key={musica.id}>
+                                    <strong>Artista:</strong>
+                                    <p>{musica.artista}</p>
+                                    <strong>Ano:</strong>
+                                    <p>{musica.ano}</p>
+                                    <strong>Álbum:</strong>
+                                    <p>{musica.album}</p>
+                                    <strong>Genero:</strong>
+                                    <p>{musica.genero}</p>
+                                    <strong>Titulo:</strong>
+                                    <p>{musica.titulo}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    ) : ''}
+                </section>
             </div>
         </div>
     )
